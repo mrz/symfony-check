a945e0373ee907b48273e33331d4c29cba4c94f9
Mattia Ziulu <mziulu@gmail.com>
2020-01-02 07:52:38 +0100

    Add dependencies.

--- mix.exs
+++ mix.exs
@@ -21,6 +21,9 @@ defmodule SymfonyVulnChecker.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:poison, "~> 3.1"},
+      {:yaml_elixir, "~> 2.4.0"},
+      {:git_cli, "~> 0.3"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
